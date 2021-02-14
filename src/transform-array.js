b6911c0fa251c06bf5fe5bd12b86c6341c7af384
Danil Rodionow <danil.rodionow13@gmail.com>
2021-02-14 18:23:16 +0300

    fix: fix transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,21 +1,44 @@
 const CustomError = require("../extensions/custom-error");
 
-// module.exports =
-function transform(arr) {
-  if (arr.every((item) => typeof item === "number")) return arr;
-  return arr
-    .join(":")
-    .replace(/--discard-next:\d+/gi, "")
-    .replace(/:--discard-next:/gi, "")
-    .replace(/\d+:--discard-prev/gi, "")
-    .replace(/:--discard-prev:/gi, "")
-    .replace(/(\d+):(--double-prev)/, "$1:$1")
-    .replace(/:(--double-prev):/, "")
-    .replace(/(--double-next):(\d+)/, "$2:$2")
-    .replace(/:(--double-next):/, "")
-    .split(":");
-}
-
-console.log(
-  transform([1, 2, 3, "--double-next", 1337, "--discard-prev", 4, 5])
-);
+module.exports = function transform(arr) {
+  let res = [...arr];
+  res.forEach((item, index, array) => {
+    if (item === "--discard-next") {
+      if (index !== res.length - 1) {
+        if (typeof res[index + 1] === "number") {
+          res.splice(index + 1, 1);
+        }
+      } else {
+        res.splice(index, 1);
+      }
+    }
+    if (item === "--double-prev") {
+      if (index !== 0) {
+        if (typeof res[index - 1] === "number") {
+          res.splice(index, 1, array[index - 1]);
+        } else {
+          res.splice(index, 1);
+        }
+      }
+    }
+    if (item === "--double-next") {
+      if (index !== res.length - 1) {
+        if (typeof res[index + 1] === "number") {
+          res.splice(index, 1, array[index + 1]);
+        } else {
+          res.splice(index, 1);
+        }
+      }
+    }
+    if (item === "--discard-prev") {
+      if (index !== 0) {
+        if (typeof res[index - 1] === "number") {
+          res.splice(index - 1, 1);
+        }
+      } else {
+        res.splice(index, 1);
+      }
+    }
+  });
+  return res.filter((item) => typeof item === "number");
+};
