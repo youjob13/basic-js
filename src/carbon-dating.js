56b221f8bd9f530145ab6aa62a21289212fc8dea
Danil Rodionow <danil.rodionow13@gmail.com>
2021-03-01 16:56:19 +0300

    fix: fix carbon-dating

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -5,9 +5,9 @@ const HALF_LIFE_PERIOD = 5730;
 
 module.exports = function dateSample(sampleActivity) {
   if (
-    typeof sampleActivity === "number" &&
-    sampleActivity > 0 && sampleActivity < 3 &&
-    !String(sampleActivity).includes('.')
+    typeof sampleActivity === "string" &&
+    +sampleActivity > 0 &&
+    +sampleActivity <= MODERN_ACTIVITY
   ) {
     return Math.ceil(
       Math.log(MODERN_ACTIVITY / sampleActivity) / (0.693 / HALF_LIFE_PERIOD)
