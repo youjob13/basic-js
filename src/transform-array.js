3757e79e5817e490c6cc89b332c52aa32051de00
Danil Rodionow <danil.rodionow13@gmail.com>
2021-03-14 14:13:10 +0300

    fix: fix transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,55 +1,30 @@
 const CustomError = require("../extensions/custom-error");
 
 module.exports = function transform(arr) {
-  function a(str, index, array) {
-    if (str === "--discard-next") {
-      if (index !== res.length - 1) {
-        if (typeof res[index + 1] === "number") {
-          res.splice(index + 1, 1);
-        }
-      } else {
-        res.splice(index, 1);
+  let res = [];
+  arr.forEach((item, index) => {
+    if (item === "--discard-prev" && arr[index - 2] !== "--discard-next") {
+      res.pop();
+    } else if (item === "--double-prev") {
+      if (index !== 0 && arr[index - 2] !== "--discard-next") {
+        res.push(arr[index - 1]);
       }
-    }
-    if (str === "--double-prev") {
-      if (index !== 0) {
-        if (typeof res[index - 1] === "number") {
-          res.splice(index, 1, array[index - 1]);
-        } else {
-          res.splice(index, 1);
-        }
-      }
-    }
-    if (str === "--double-next") {
-      if (index !== res.length - 1) {
-        if (typeof res[index + 1] === "number") {
-          res.splice(index, 1, array[index + 1]);
-        } else {
-          res.splice(index, 1);
-        }
-      }
-    }
-    if (str === "--discard-prev") {
-      if (index !== 0) {
-        if (typeof res[index - 1] === "number") {
-          res.splice(index - 1, 1);
-        }
-      } else {
-        res.splice(index, 1);
-      }
-    }
-  }
-
-  let res = [...arr];
-  res.forEach((item, index, array) => {
-    if (
-      item === "--double-next" ||
-      item === "--double-prev" ||
-      item === "--discard-next" ||
-      "--discard-prev"
+    } else if (arr[index - 1] === "--double-next") {
+      res.push(item);
+      res.push(item);
+    } else if (
+      "--double-next" === item ||
+      arr[index - 1] === "--discard-next"
     ) {
-      a(item, index, array);
+      return;
+    } else if (
+      item !== "--double-next" &&
+      item !== "--discard-next" &&
+      item !== "--double-prev" &&
+      item !== "--discard-prev"
+    ) {
+      res.push(item);
     }
   });
-  return res.filter((item) => typeof item === "number");
+  return res;
 };
